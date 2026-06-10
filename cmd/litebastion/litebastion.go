15053c7f1e7c832366227a18bc92daacba516aa0
Filippo Valsorda <hi@filippo.io>
2026-06-10 14:20:18 +0200

    Also ignore lines of whitespace

--- cmd/litebastion/litebastion.go
+++ cmd/litebastion/litebastion.go
@@ -105,7 +105,7 @@ func main() {
 		}
 		bs := strings.TrimSpace(string(backendsList))
 		for _, line := range strings.Split(bs, "\n") {
-			if line == "" || line[0] == '#' {
+			if strings.TrimSpace(line) == "" || line[0] == '#' {
 				continue
 			}
 			l, err := hex.DecodeString(line)
